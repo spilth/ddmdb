46e26ed969cfe8e7b9e89c0da469ef244e0f3a19
Brian Kelly <polymonic@gmail.com>
2013-12-18 20:22:26 -0500

    Removed whitespace that RBP was complaining about.

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,7 +2,7 @@ require 'rollbar/rails'
 Rollbar.configure do |config|
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
-  # Without configuration, Rollbar is enabled by in all environments. 
+  # Without configuration, Rollbar is enabled by in all environments.
   # To disable in specific environments, set config.enabled=false.
   # Here we'll disable in 'test':
   if Rails.env.test?
